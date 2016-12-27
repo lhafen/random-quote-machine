6dfcc639b85c5cd0e845f094fd0455f6c3b75ba0
Lara Hafen <larahafen@gmail.com>
2016-12-27 10:06:05 -0800

    Update script.js

--- script.js
+++ script.js
@@ -2,16 +2,7 @@ document.body.addEventListener('load', changeQuote());
 
 function changeQuote(){
 
-    var previousNum;
-
-    while(previousNum === randomNum){
-    var randomNum = Math.floor((Math.random() * 30) + 0);
-}
-
-var randomNum = Math.floor((Math.random() * 30) + 0);
-
-previousNum = randomNum;
-
+//Array of quotes
 var quotesArr =     [["What one man can do, another can do","Unknown"],
                      ["If you can take it, you can make it", "Unknown"],
                      ["If you can dream it, you can do it.","Walt Disney"],
@@ -21,11 +12,11 @@ var quotesArr =     [["What one man can do, another can do","Unknown"],
                      ["If not us, who? If not now, when?", "John F. Kennedy"],
                      ["I can, therefore I am.", "Simone Weil"],
                      ["Wherever you go, go with all your heart.","Confucius"],
-                     ["Do what you can, with what you have, where you are.","Theordore Roosevelt"],
+                     ["Do what you can, with what you have, where you are.","Theodore Roosevelt"],
                      ["Action is the foundational key to all success.","Pablo Picasso"],
-                     ["Do one thing every day that scares you.","Eleanor Roosevelt"]
+                     ["Do one thing every day that scares you.","Eleanor Roosevelt"],
                      ["You must do the thing you think you cannot do.","Eleanor Roosevelt"],
-                     ["Believe you can and you&#39;re halfway there.","Theodore Roosevelt"],
+                     ["Believe you can and you&#39;re halfway there.","Theordore Roosevelt"],
                      ["Eighty percent of success is showing up.","Woody Allen"],
                      ["To be the best, you must be able to handle the worst.","Wilson Kanadi"],
                      ["A jug fills drop by drop.","Buddha"],
@@ -43,9 +34,21 @@ var quotesArr =     [["What one man can do, another can do","Unknown"],
                      ["The ones who are crazy enough to think they can change the world, are the ones that do.","Anonymous"],
                      ["Don&#39;t be afraid to give up the good to go for the great.","John D. Rockefeller"],
                      ["No masterpiece was ever created by a lazy artist.","Anonymous"]];
+
+//Get a random number between 0 and the array length
+var randomNum = Math.floor((Math.random() * quotesArr.length));
+
+//Set quote and author based on random number
+console.log(randomNum);
 var showQuote = quotesArr[randomNum][0];
 var showAuthor = quotesArr[randomNum][1];
+
+//Display random quote and author
 document.getElementById("quoteText").innerHTML = showQuote;
 document.getElementById("author").innerHTML = showAuthor;
 
+//Update Twitter button with quote text
+document.getElementById("twitter-share-button").setAttribute("href", "http://twitter.com/share?text=" + showQuote + "%20%23motivationquote");
+
                  };
+
